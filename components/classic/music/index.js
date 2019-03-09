3acfa0e1b988b7b2c2b01a8f187d29cc86da22c3
TheLastDark <yinghuo68@163.com>
2019-03-09 12:59:36 +0800

    完善music组件

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,25 +1,80 @@
-import {classicBeh} from '../classic-beh.js'
+import {
+  classicBeh
+} from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
 Component({
   /**
    * 组件的属性列表
    */
   behaviors: [classicBeh],
   properties: {
+    src: String,
+    title:String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: '../../images/player@waitting.png',
-    playSrc: '../../images/player@playing.png'
+    playing: false,
+    pauseSrc: '../../images/player@pause.png',
+    playSrc: '../../images/player@play.png'
   },
 
+  // 生命周期函数
+  attached:function(){
+    this._recoverStatus()
+    this._monitorSwitch()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay: function (event) {
+      if (!this.data.playing) {
+        this.setData({
+          playing: !(this.data.playing)
+        })
+        mMgr.src = this.properties.src
+        mMgr.title = this.properties.title
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    },
 
+    _recoverStatus: function(){
+      if(mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+        // 退出函数不然两个判断都要执行
+      }
+
+      if(mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+
+    _monitorSwitch: function(){
+      mMgr.onPlay(()=>{
+        this._recoverStatus()
+      })
+      mMgr.onStop(()=>{
+        this._recoverStatus()
+      })
+      mMgr.onPause(()=>{
+        this._recoverStatus()
+      })
+      mMgr.onEnded(()=>{
+        this._recoverStatus()
+      })
+    }
   }
 })
