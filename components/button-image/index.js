d2afdc3f79b9795e4cef6cf4636c9946b539106c
TheLastDark <yinghuo68@163.com>
2019-05-16 16:35:55 +0800

    my

---
+++ components/button-image/index.js
@@ -0,0 +1,30 @@
+// components/button-image/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    openType: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    getUserinfo(event) {
+      this.triggerEvent('getuserinfo',event.detail,{})
+    }
+  }
+})
